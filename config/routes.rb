fe28ac79c4bc0b0957cedffc8dda168f47f8c32d
KT <TEnZII843>
2023-07-26 16:13:26 +0600

    feat:update

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   root "posts#index"
   resources :posts
+
+  # get "/karma", to: "posts#karma"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # get '/articles/download', to: 'articles#download', as: :article_download
 end
